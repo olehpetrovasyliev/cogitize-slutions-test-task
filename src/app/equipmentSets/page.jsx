321e5f5c3da4e1c30e5a596690e31f82047cd16a
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-11-23 22:37:41 +0200

    add routes and first styles

---
+++ src/app/equipmentSets/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const EquipmentSets = () => {
+  return (
+    <p>Извините, эта страница не была включена в задачу моего создателя</p>
+  );
+};
+
+export default EquipmentSets;
